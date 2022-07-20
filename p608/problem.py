a5b020816c52d6241adb2d29fd704e191639d3f7
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-07-20 23:06:31 +0400

    [608]: given test set is passing

--- p608/problem.py
+++ p608/problem.py
@@ -1,23 +1,33 @@
-from pprint import pprint
-
-
-def decide(lines: list[str]):
-    fair_coins = [False for _ in range(0, 11 + 1)]
+def decide(lines: list[str]) -> dict:
+    fair_coins = {chr(letter): False for letter in range(ord('A'), ord('L') + 1)}
     assert len(fair_coins) == 12, len(fair_coins)
-    for line in lines:
-        if line.endswith("even"):
-            fair_coin_letters = "".join(line.split(" ")[0:2])
-            for letter in fair_coin_letters:
-                fair_coins[ord(letter)-ord('A')] = True
+    for line in filter(lambda x: x.endswith("even"), lines):
+        fair_coin_letters = "".join(line.split(" ")[0:2])
+        for letter in fair_coin_letters:
+            fair_coins[letter] = True
 
-    pprint(fair_coins)
+    for line in filter(lambda x: not x.endswith("even"), lines):
+        remaining_letters = dict(filter(lambda xy: not xy[1], fair_coins.items()))
+        candidate = None
+        for remaining_letter in remaining_letters:
+            if remaining_letter in line:
+                candidate = remaining_letter
+
+        assert candidate is not None
+        if line.endswith("up"):
+            result = "light"
+        else:
+            result = "heavy"
+
+        return {'coin': candidate, 'status': result}
 
 
 def split_by_cases(lines: list[str]) -> list[list[str]]:
-    result = [[]]
+    empty_group = []
+    result = [empty_group]
     for line in lines:
         if line == "":
-            result.append([])
+            result.append(empty_group)
         else:
             result[-1].append(line)
     return result
@@ -37,7 +47,8 @@ def start(file_name):
     groups = split_by_cases(lines)
 
     for group in groups:
-        decide(group)
+        result = decide(group)
+        print(f"{result['coin']} is the counterfeit coin and it is {result['status']}.")
         completed += 1
 
     assert completed == total
